25626b6dc33bb423150f0dc396b6f2a1513c239d
Ilmir Bakirov <ilmirbakirov@gmail.com>
2016-07-05 21:05:13 +0500

    add java based config

--- Config/JavaBasedConfig/src/main/java/com/bakigoal/JavaMain.java
+++ Config/JavaBasedConfig/src/main/java/com/bakigoal/JavaMain.java
@@ -3,7 +3,6 @@ package com.bakigoal;
 import com.bakigoal.config.AppConfig;
 import com.bakigoal.model.Movie;
 import com.bakigoal.service.MovieService;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 /**
@@ -11,10 +10,13 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
  */
 public class JavaMain {
   public static void main(String[] args) {
-    ConfigurableApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+    AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+    context.register(AppConfig.class);
+//    context.scan("com.bakigoal.dao");
+    context.refresh();
 
-    MovieService movieQualifierService = context.getBean(MovieService.class);
-    printUsers(movieQualifierService, "!!! MovieDao: !!!");
+    MovieService movieService = context.getBean(MovieService.class);
+    printUsers(movieService, "!!! MovieDao: !!!");
 
     context.registerShutdownHook();
   }
--- Config/JavaBasedConfig/src/main/java/com/bakigoal/config/AppConfig.java
+++ Config/JavaBasedConfig/src/main/java/com/bakigoal/config/AppConfig.java
@@ -1,5 +1,8 @@
 package com.bakigoal.config;
 
+import com.bakigoal.dao.MovieDao;
+import com.bakigoal.dao.impl.MovieDaoImpl;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
@@ -7,7 +10,11 @@ import org.springframework.context.annotation.Configuration;
  * Created by ilmir on 05.07.16.
  */
 @Configuration
-@ComponentScan(basePackages = {"com.bakigoal.service", "com.bakigoal.dao"})
+@ComponentScan(basePackages = "com.bakigoal.service")
 public class AppConfig {
 
+  @Bean
+  MovieDao movieDao(){
+    return new MovieDaoImpl();
+  }
 }
