b29fc037a6dcc696c6acf51ca68cd931bd143047
Ilmir Bakirov <ilmirbakirov@gmail.com>
2016-07-05 23:13:26 +0500

    spring converter service

---
+++ Config/Converter/src/main/java/com/bakigoal/ConverterMain.java
@@ -0,0 +1,20 @@
+package com.bakigoal;
+
+import com.bakigoal.service.MyService;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Created by ilmir on 05.07.16.
+ */
+public class ConverterMain {
+  public static void main(String[] args) {
+    ConfigurableApplicationContext context = new ClassPathXmlApplicationContext(new String[]{"beans.xml"});
+
+    MyService myService = context.getBean(MyService.class);
+    System.out.println(myService.convertStringToDouble("123"));
+    System.out.println(myService.convertStringsToList(new String[]{"123", "312", "321"}));
+
+    context.registerShutdownHook();
+  }
+}
---
+++ Config/Converter/src/main/java/com/bakigoal/service/MyService.java
@@ -0,0 +1,28 @@
+package com.bakigoal.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.convert.ConversionService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * Created by ilmir on 05.07.16.
+ */
+@Service
+public class MyService {
+  private final ConversionService conversionService;
+
+  @Autowired
+  public MyService(ConversionService conversionService) {
+    this.conversionService = conversionService;
+  }
+
+  public Double convertStringToDouble(String s) {
+    return this.conversionService.convert(s, Double.class);
+  }
+
+  public List convertStringsToList(String[] s) {
+    return this.conversionService.convert(s, List.class);
+  }
+}
